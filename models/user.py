49114fb0a5ec1bdeff695ad8cb73d8d7fe8c83ff
Mariam Hasanat <hasanattmariam@gmail.com>
2025-06-12 20:21:23 +0300

    feat: create user class

---
+++ models/user.py
@@ -0,0 +1,26 @@
+from libraryItem import LibraryItem
+
+class User:
+    def __init__(self, user_id: int, name: str, email: str, borrowed_items: list[LibraryItem] = []):
+        self.__user_id = user_id
+        self.__name = name
+        self.__email = email
+        self.__borrowed_items = borrowed_items
+
+    def get_user_id(self) -> int:
+        return self.__user_id
+
+    def get_name(self) -> str:
+        return self.__name
+
+    def get_email(self) -> str:
+        return self.__email
+
+    def get_borrowed_items(self) -> list:
+        return self.__borrowed_items
+
+    def add_borrowed_item(self, item):
+        self.__borrowed_items.append(item)
+
+    def remove_borrowed_item(self, item):
+        self.__borrowed_items.remove(item)
