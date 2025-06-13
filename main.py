96574c1e7150c270a342dae4a79b2ca066112da0
Mariam Hasanat <hasanattmariam@gmail.com>
2025-06-13 11:34:47 +0300

    feat: enhance user data handling by appending user information to JSON file

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
+import json
 from models.dvd import DVD
+from models.user import User
+import os
 
 if __name__ == "__main__":
+    path = "users.json"
+
+    if os.path.exists(path) and os.stat(path).st_size > 0:
+        with open(path, "r") as file:
+            data = json.load(file)
+    else:
+        data = []
+
     dvd = DVD("Inception", "Christopher Nolan", 2010, "available", 148)
+    dvd2 = DVD("Inception", "Christopher Nolan", 2010, "available", 148)
+    dvd.set_status("borrowed")
+    dvd2.set_status("borrowed")
+    user = User(1, "John Doe", "john.doe@example.com", [dvd, dvd2])
+    up_user = user.to_dict()
+
+    data.append(up_user)
+
+    with open(path, "w") as file:
+        json.dump(data, file, indent=4)
+
     print(dvd.get_status())
     print(dvd.display_info())
     print(dvd.check_availability())
-  
-    
-    # Example of using the DVD class
     print(f"DVD Title: {dvd.get_title()}")
     print(f"DVD Duration: {dvd.get_duration()} minutes")
